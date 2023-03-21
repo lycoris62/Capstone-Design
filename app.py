e26148e6bac6eaa67b943139a1f8ffa6d58e1432
Lycoris62 <wodbs7758@naver.com>
2023-03-21 12:43:00 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,26 @@
+import cv2
+from flask import Flask, Response, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return render_template('index.html')
+
+def gen_frames():  
+    camera = cv2.VideoCapture(0)
+    while True:
+        success, frame = camera.read()
+        if not success:
+            break
+        frame = cv2.flip(frame, 1)
+        ret, buffer = cv2.imencode('.jpg', frame)
+        frame = buffer.tobytes()
+        yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    app.run()
